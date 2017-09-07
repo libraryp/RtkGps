db1a265c8334548343bf977f3f4d10c9cd8363b0
Libraryp <2045411+libraryp@users.noreply.github.com>
2017-09-08 04:05:46 +0900

    fix: delete SiRF IV support

--- jni/rtklib.mk
+++ jni/rtklib.mk
@@ -53,7 +53,6 @@ LOCAL_SRC_FILES += \
         $(RTKLIB_PATH)/src/rcv/nvs.c \
         $(RTKLIB_PATH)/src/rcv/rcvlex.c \
         $(RTKLIB_PATH)/src/rcv/skytraq.c \
-        $(RTKLIB_PATH)/src/rcv/sirf.c \
         $(RTKLIB_PATH)/src/rcv/ss2.c \
         $(RTKLIB_PATH)/src/rcv/ublox.c \
 	$(RTKLIB_PATH)/src/rcv/rt17.c
